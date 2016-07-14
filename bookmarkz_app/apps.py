0efed2318934e0c4bc9478cb3d68a0131f0027c8
Priyanka Nath <pnath.it@gmail.com>
2016-07-14 12:58:28 +0530

    changed name (bookmarks->bookmarkz)

---
+++ bookmarkz_app/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class BookmarkzAppConfig(AppConfig):
+    name = 'bookmarkz_app'
---
+++ bookmarkz_app/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Link(models.Model):
+  url = models.URLField(unique=True)
+
+class Bookmark(models.Model):
+  title = models.CharField(max_length=200)
+  user = models.ForeignKey(User)
+  link = models.ForeignKey(Link)

19daba6df3cabe8e71d05ad14af5cf96e240f969
Priyanka Nath <pnath.it@gmail.com>
2016-11-08 12:49:03 +0530

    added views in bookmarkz_app

--- bookmarkz/urls.py
+++ bookmarkz/urls.py
@@ -13,16 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bookmarkz_app'))
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.static import serve
 from django.contrib.auth import views as auth_views
-from bookmarkz import views
-import os
+from bookmarkz import views as bookmarkz_views
+from bookmarkz_app import views as bookmarkz_app_views
 
 #pathname manipulation
 site_media = os.path.join(
-  os.path.dirname(__file__),'../', 'site_media'
+  os.path.dirname(__file__),'..', 'site_media'
 )
 
 #path is bookmarkz/bookmarkz
@@ -31,16 +36,16 @@ urlpatterns = [
     url(r'^site_media/(?P<path>.*)$', serve,
      { 'document_root': site_media }),
     # Browsing
-    url(r'^$', views.main_page, name='main_page'),
-    url(r'^user/(\w+)/$', views.user_page, name='user_page'),
+    url(r'^$', bookmarkz_app_views.main_page, name='main_page'),
+    url(r'^user/(\w+)/$', bookmarkz_app_views.user_page, name='user_page'),
     #url(r'^tag/([^\s]+)/$', tag_page, name='tag_page'),
-    url(r'^tag/$', views.tag_cloud_page),
-    url(r'^search/$', views.search_page),
+    url(r'^tag/$', bookmarkz_app_views.tag_cloud_page),
+    url(r'^search/$', bookmarkz_app_views.search_page),
     # Session management
     url(r'^accounts/login/$', auth_views.login ),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', views.logout_page, name='logout'),
-    url(r'^register/$', views.register, name='register'),
+    url(r'^logout/$', bookmarkz_views.logout_page, name='logout'),
+    url(r'^register/$', bookmarkz_views.register, name='register'),
     # Account management
-    url(r'^save/$', views.bookmark_save_page, name='bookmark_save'),
+    url(r'^save/$', bookmarkz_views.bookmark_save_page, name='bookmark_save'),
 ]

db4ae74d9936cf437de77f36d01aa280e9a1fbe8
Priyanka Nath <pnath.it@gmail.com>
2017-05-07 18:31:15 +0530

    specified host

--- bookmarkz/settings.py
+++ bookmarkz/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bookmarkzdb',
-        'HOST': '127.0.0;1',
+        'HOST': '127.0.0.1',
     }
 }
 

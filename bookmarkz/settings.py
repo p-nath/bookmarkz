32120b77be44767d527bd3aa2a762342173af1a2
Priyanka Nath <pnath.it@gmail.com>
2017-05-07 19:00:03 +0530

    set engine

--- bookmarkz/settings.py
+++ bookmarkz/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 DATABASES = {'default': dj_database_url.config()}
-
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
